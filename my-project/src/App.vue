373614ea75e9b6d762b8006df21136b54cc408bb
Your Name <you@example.com>
2016-11-20 20:24:59 +0800

    '2016/11/20'

---
+++ my-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+     <vheader></vheader>
+     <div class="tab">
+       <div class="tab-item"><a v-link="{ path:'/goods'}">商品</a></div>
+       <div class="tab-item"><a v-link="{ path:'/ratings'}">评论</a></div>
+       <div class="tab-item"><a v-link="{ path:'/seller'}">商家</a></div>
+     </div>
+     <router-view></router-view>
+  </div>
+</template>
+
+<script>
+    import vheader from './components/header/header'
+
+    export default {
+        components: {
+            vheader: vheader
+        }
+    }
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+    .tab 
+       display: flex 
+       width: 100% 
+       height: 40px
+       line-height: 40px
+       .tab-item
+          flex: 1
+          text-align: center
+          &>a
+            display:block
+            text-decoration:none
+            font-size: 14x
+            color:rbg(77,85,93)
+            &.active
+               color: rgb(240, 20 ,20)
+</style>
