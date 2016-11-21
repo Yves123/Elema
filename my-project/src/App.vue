4c1639d211e269cfe47dfff5e241cea3581aab8c
Your Name <you@example.com>
2016-11-21 22:16:41 +0800

    '2016/11/21'

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-     <vheader></vheader>
+     <vheader :seller='seller'></vheader>
      <div class="tab">
        <div class="tab-item"><a v-link="{ path:'/goods'}">商品</a></div>
        <div class="tab-item"><a v-link="{ path:'/ratings'}">评论</a></div>
@@ -13,27 +13,50 @@
 <script>
     import vheader from './components/header/header'
 
+    const ERR_OK = 0
+
     export default {
-        components: {
-            vheader: vheader
-        }
+        data() {
+                return {
+                    seller: {}
+                }
+            },
+            created() {
+                this.$http.get('/api/seller').then(function(response) {
+                    response = response.body
+                    if (response.erron === 0) {
+                        this.seller = response.data
+                    }
+                })
+            },
+            components: {
+                vheader: vheader
+            }
     }
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
-    .tab 
-       display: flex 
-       width: 100% 
-       height: 40px
-       line-height: 40px
-       .tab-item
-          flex: 1
-          text-align: center
-          &>a
-            display:block
-            text-decoration:none
-            font-size: 14x
-            color:rbg(77,85,93)
-            &.active
-               color: rgb(240, 20 ,20)
+    .tab {
+        display: flex;
+        display: flex;
+        width: 100%;
+        height: 40px;
+        line-height: 40px
+    }
+    
+    .tab .tab-item {
+        flex: 1;
+        text-align: center
+    }
+    
+    .tab .tab-item a {
+        display: block;
+        text-decoration: none;
+        font-size: 14x;
+        color: rbg(77, 85, 93)
+    }
+    
+    .tab .tab-item a.active {
+        color: rgb(240, 20, 20)
+    }
 </style>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
 import goods from './components/goods/goods'
 import ratings from './components/ratings/ratings'
 import seller from './components/seller/seller'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 let app = Vue.extend(App)
 
 let router = new VueRouter({
-  linkActiveClass: 'active'
+    linkActiveClass: 'active'
 })
 
 router.map({
